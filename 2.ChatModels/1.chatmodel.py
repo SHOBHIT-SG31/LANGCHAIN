aa982d79372536df5823ba4c494358b05d6f8286
SHOBHIT-SG31 <shobhitgupta539@gmail.com>
2026-06-13 23:09:25 +0530

    Updated

--- 2.ChatModels/1.chatmodel.py
+++ 2.ChatModels/1.chatmodel.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
+model = ChatGoogleGenerativeAI(model="gemini-3-flash-preview",temperature=1.8)
 
-results = model.invoke("What is the capital of India")
-print(results)
+results = model.invoke("Suggest me 5 indian person names")
+print(results.content)
