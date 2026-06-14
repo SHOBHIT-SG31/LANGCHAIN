9a92a422e904b68c48e06a457271e60f83c365f7
SHOBHIT-SG31 <shobhitgupta539@gmail.com>
2026-06-14 15:50:04 +0530

    Updated

---
+++ 3.EmbeddingModels/3.document_similarity.py
@@ -0,0 +1,22 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+embedding = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", output_dimensionality=300)
+
+document=[
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = "Tell me about Virat Kohli"
+
+doc_embedding = embedding.embed_documents(document)
+query_embedding = embedding.embed_query(query)
+
+print(cosine_similarity([query_embedding],doc_embedding))
