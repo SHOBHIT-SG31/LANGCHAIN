256411bb69f6b46de69d1f151410b8cf38e09ef7
SHOBHIT-SG31 <shobhitgupta539@gmail.com>
2026-07-21 16:33:22 +0530

    initial commt

---
+++ Langchain_Runnables/pdf_reader.py
@@ -0,0 +1,6 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+
