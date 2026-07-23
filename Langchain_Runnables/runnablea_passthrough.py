4193602c65def64278d0d2becf5b114da31228e9
SHOBHIT-SG31 <shobhitgupta539@gmail.com>
2026-07-23 14:31:36 +0530

    added

---
+++ Langchain_Runnables/runnablea_passthrough.py
@@ -0,0 +1,27 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-3.1-flash-lite')
+
+prompt1 = PromptTemplate(
+    template='Write a joke on {topic}',
+    input_variables=['topic']
+)
+prompt2 = PromptTemplate(
+    template = 'Explain the following joke {text}',
+    input_variables=['text']
+)
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+parallel_chain = RunnableParallel({
+    'joke' : RunnablePassthrough(), # RunnablePassthrough means output is same as input
+    'explanation' : RunnableSequence(prompt2,model,parser)
+})
+final_chain = RunnableSequence(joke_gen_chain,parallel_chain)
+print(final_chain.invoke({'topic':'cricket'}))
