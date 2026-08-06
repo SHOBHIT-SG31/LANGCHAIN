47bf77c166e5a325287d3153a8219f95a1957224
SHOBHIT-SG31 <shobhitgupta539@gmail.com>
2026-08-06 15:37:51 +0530

    modified

--- langchain_document_loaders/directory_loader.py
+++ langchain_document_loaders/directory_loader.py
@@ -1,8 +1,9 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-loader = DirectoryLoader(
+# from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_document_loaders import directory_loader, pdf_loader
+loader = directory_loader(
     path='books',
     glob='*.pdf',
-    loader_cls=PyPDFLoader
+    loader_cls=pdf_loader
 )
 
 # docs = loader.load()
